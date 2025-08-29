2564694aaf965629b3f43970eb6f591d63c964b2
FedericoMaciel <federico.maciel@abitab.com.uy>
2025-08-28 22:08:21 -0300

    implement web push notifications

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,4 +1,16 @@
+import React from "react";
 import { Stack } from "expo-router";
+import { Platform } from "react-native";
+
+if (Platform.OS === "web") {
+  if ("serviceWorker" in navigator)
+    navigator.serviceWorker
+      .register("/firebase-messaging-sw.js")
+      .then((reg) => console.log("Service worker registered:", reg))
+      .catch((err) =>
+        console.error("Service worker registration failed:", err)
+      );
+}
 
 export default function RootLayout() {
   return <Stack />;
--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,8 +1,6 @@
-import { useState, useEffect, useRef } from 'react';
-import { Text, View, Button, Platform } from 'react-native';
-import * as Device from 'expo-device';
-import * as Notifications from 'expo-notifications';
-import Constants from 'expo-constants';
+import React, { useState, useEffect } from "react";
+import { Text, View, Platform } from "react-native";
+import * as Notifications from "expo-notifications";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -14,53 +12,55 @@ Notifications.setNotificationHandler({
 });
 
 async function registerForPushNotificationsAsync() {
-  if (Platform.OS === 'android') {
-    await Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
+  if (Platform.OS === "android") {
+    await Notifications.setNotificationChannelAsync("default", {
+      name: "default",
       importance: Notifications.AndroidImportance.MAX,
       vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
+      lightColor: "#FF231F7C",
     });
   }
 
-    const { status: existingStatus } = await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
-    if (existingStatus !== 'granted') {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
-    }
-    if (finalStatus !== 'granted') {
-      console.log('Permission not granted')
-      return;
-    }
-    try {
-      const pushTokenString = (
-        await Notifications.getDevicePushTokenAsync()
-      ).data;
-      console.log(pushTokenString);
-      return pushTokenString;
-    } catch (e: unknown) {
-      console.log(e)
-    }
- 
+  const { status: existingStatus } = await Notifications.getPermissionsAsync();
+  let finalStatus = existingStatus;
+  if (existingStatus !== "granted") {
+    const { status } = await Notifications.requestPermissionsAsync();
+    finalStatus = status;
+  }
+  if (finalStatus !== "granted") {
+    console.log("Permission not granted");
+    return;
+  }
+  try {
+    const pushTokenString = (await Notifications.getDevicePushTokenAsync())
+      .data;
+    console.log(pushTokenString);
+    return pushTokenString;
+  } catch (e: unknown) {
+    console.log(e);
+  }
 }
 
-
 export default function Index() {
-  const [notificationToken, setNotificationToken] = useState('')
-  const [notification, setNotification] = useState<Notifications.Notification | undefined>(
-    undefined
-  );
-   useEffect(() => {
-    registerForPushNotificationsAsync().then(token => setNotificationToken(token))
+  const [notificationToken, setNotificationToken] = useState("");
+  const [notification, setNotification] = useState<
+    Notifications.Notification | undefined
+  >(undefined);
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setNotificationToken(token)
+    );
 
-    const notificationListener = Notifications.addNotificationReceivedListener(notification => {
-      setNotification(notification);
-    });
+    const notificationListener = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        setNotification(notification);
+      }
+    );
 
-    const responseListener = Notifications.addNotificationResponseReceivedListener(response => {
-      console.log(response);
-    });
+    const responseListener =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
 
     return () => {
       notificationListener.remove();
@@ -69,12 +69,19 @@ export default function Index() {
   }, []);
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-around' }}>
+    <View
+      style={{ flex: 1, alignItems: "center", justifyContent: "space-around" }}
+    >
       <Text>Your push token: {notificationToken}</Text>
-      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Title: {notification && notification.request.content.title} </Text>
+      <View style={{ alignItems: "center", justifyContent: "center" }}>
+        <Text>
+          Title: {notification && notification.request.content.title}{" "}
+        </Text>
         <Text>Body: {notification && notification.request.content.body}</Text>
-        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+        <Text>
+          Data:{" "}
+          {notification && JSON.stringify(notification.request.content.data)}
+        </Text>
       </View>
     </View>
   );
